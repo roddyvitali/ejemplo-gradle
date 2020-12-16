119ce83edbedc7fd85bc34a5ad9b498339580174
Roddy Vitali <roddicks@gmail.com>
2020-12-15 21:49:12 -0300

    ci(Jenkinsfile): fix filePath maven

--- maven.groovy
+++ maven.groovy
@@ -22,7 +22,7 @@ def call(){
         sh 'curl -X GET http://localhost:8888/rest/mscovid/test?msg=testing'
     }
     stage('Nexus') {
-        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: './build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: './build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
     }
 }
 
